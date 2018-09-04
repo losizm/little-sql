d3ba87427c8bcbb5b811e9ff2b608878d1e2cbee
Carlos Conyers <carlos.conyers@hotmail.com>
2018-09-03 22:05:57 -0400

    Updated tests

--- src/test/scala/little/sql/SqlSpec.scala
+++ src/test/scala/little/sql/SqlSpec.scala
@@ -21,26 +21,22 @@ import java.sql.Connection
 import Implicits._
 
 class SqlSpec extends FlatSpec {
-  var conn: Connection = null
-   
-  "Connection" should "be created" in {
-    conn = Connector(s"""jdbc:h2:${sys.props("java.io.tmpdir")}/test""", "sa", "", "org.h2.Driver").getConnection()
-  }
+  private val connector = Connector(s"jdbc:h2:${sys.props("java.io.tmpdir")}/test", "sa", "", "org.h2.Driver")
 
-  it should "drop table if it exists" in {
+  "SQL statement" should "drop table if it exists" in connector.withConnection { conn =>
     conn.update("drop table prog_lang if exists")
   }
 
-  it should "create table" in {
+  it should "create table" in connector.withConnection { conn =>
     conn.update("create table prog_lang (id int, name text, comments text)")
   }
 
-  it should "insert records into table" in {
+  it should "insert records into table" in connector.withConnection { conn =>
     val count = conn.update("insert into prog_lang(id, name) values (1, 'basic'), (2, 'pascal'), (3, 'c')")
     assert(count == 3)
   }
 
-  it should "select records from table" in {
+  it should "select records from table" in connector.withConnection { conn =>
     conn.forEachRow("select * from prog_lang") { rs =>
       val id = rs.get[Int]("id")
       val name = rs.get[String]("name")
@@ -48,7 +44,7 @@ class SqlSpec extends FlatSpec {
     }
   }
 
-  it should "select record having one column with null value" in {
+  it should "select record having one column with null value" in connector.withConnection { conn =>
     conn.forEachRow("select * from prog_lang") { rs =>
       val id = rs.get[Option[Int]]("id")
       val name = rs.get[Option[String]]("name")
@@ -60,15 +56,11 @@ class SqlSpec extends FlatSpec {
     }
   }
 
-  it should "insert records into table with null value" in {
+  it should "insert records into table with null value" in connector.withConnection { conn =>
     conn.update("insert into prog_lang (id, name) values (?, ?)", Seq(None, "cobol"))
     val count: Option[Int] = conn.mapFirstRow("select count(*) from prog_lang where id is null") { rs =>
       rs.get[Int](1)
     }
     assert(count.getOrElse(0) == 1)
-  }
-
-  it should "be closed" in {
-    conn.close()
   }
 }
